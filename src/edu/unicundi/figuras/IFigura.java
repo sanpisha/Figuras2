4c310d2c77c0439b7f4b872fb0a82b8884d098a2
sago9 <sago9@LAPTOP-V0DH3NPL>
2020-03-05 19:15:17 -0500

    Se agrega interface Ifigura

---
+++ src/edu/unicundi/figuras/IFigura.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unicundi.figuras;
+
+/**
+ *
+ * @aSantiago Gómez
+ * clase  interface de figura 
+ */
+public interface IFigura { 
+    /**
+     * Halla el area de las figuras
+     */
+      public double hallarArea();
+      public abstract double hallarPerimetro(); 
+    
+}
